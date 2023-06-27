6f1efb159268526182a4ab5e01f057a7ef010912
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-06-27 13:12:03 +0430

    js DOM completed for the burgerMenue

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-
 // Dom manipulation For the burgerMenu
-let burgerMenu =document.querySelector('.bx-menu');
-let divBurgerMenueItem=document.querySelector('.burger-menue');
-burgerMenu.addEventListener('click',function(){
- divBurgerMenueItem.style.display='flex';
+
+// For showing the burger menu page
+
+let burgerMenu = document.querySelector(".bx-menu");
+let divBurgerMenueItem = document.querySelector(".burger-menue");
+burgerMenu.addEventListener("click", function () {
+  divBurgerMenueItem.style.display = "flex";
 });
 
-let cross=document.querySelector('.bx-x');
-cross.addEventListener('click',function(){
-    divBurgerMenueItem.style.display='none';
+//for hiding the burgerMenu page using X
+
+let cross = document.querySelector(".bx-x");
+cross.addEventListener("click", function () {
+  divBurgerMenueItem.style.display = "none";
 });
 
-let LiInMenu=document.querySelector('.burger-menue ul li');
-LiInMenu.addEventListener('click',function(){
-    divBurgerMenueItem.style.display='none';
+// for hiding the burgerMenu page, clicking other parts of page
+
+let divAll=document.querySelector(".burger-menue");
+divAll.addEventListener('click',function(){
+    divBurgerMenueItem.style.display = "none";
 })
+//for making the links in order to work
 
+let LiInMenu = document.getElementsByClassName("menue");
+for (let i = 0; i < LiInMenu.length; i++) {
+  LiInMenu[i].addEventListener('click',function(){
+      divBurgerMenueItem.style.display='none';
+  })
+}
