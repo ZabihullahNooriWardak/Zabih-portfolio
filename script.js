6c40e89ae6a3f9488e9af925a8abcb97e8170c52
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-06-27 22:22:32 +0430

    linter js problem

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // Dom manipulation For the burgerMenu
 
-// For showing the burger menu page
+//My Function For Dom manipulation of the burgerMenu
 
-const burgerMenu = document.querySelector('.bx-menu');
-const divBurgerMenueItem = document.querySelector('.burger-menue');
-burgerMenu.addEventListener('click', () => {
-  divBurgerMenueItem.style.display = 'flex';
-});
-
-// My Function For Dom manipulation of the burgerMenu
-
-const hiding = function () {
-  divBurgerMenueItem.style.display = 'none';
-};
-
-// for hiding the burgerMenu page using X
-
-const cross = document.querySelector('.bx-x');
-cross.addEventListener('click', hiding);
-
-// for hiding the burgerMenu page, clicking other parts of page
-
-const divAll = document.querySelector('.burger-menue');
-divAll.addEventListener('click', hiding);
-
-// for making the links in order to work
-
-const LiInMenu = document.getElementsByClassName('menue');
-for (let i = 0; i < LiInMenu.length; i += i) {
-  LiInMenu[i].addEventListener('click', hiding);
-}
+let hiding = function () {
+    divBurgerMenueItem.style.display = "none";
+  };
+  
+  // For showing the burger menu page
+  
+  let burgerMenu = document.querySelector(".bx-menu");
+  let divBurgerMenueItem = document.querySelector(".burger-menue");
+  burgerMenu.addEventListener("click", function () {
+    divBurgerMenueItem.style.display = "flex";
+  });
+  
+  //for hiding the burgerMenu page using X
+  
+  let cross = document.querySelector(".bx-x");
+  cross.addEventListener("click", hiding);
+  
+  // for hiding the burgerMenu page, clicking other parts of page
+  
+  let divAll = document.querySelector(".burger-menue");
+  divAll.addEventListener("click", hiding);
+  
+  //for making the links in order to work
+  
+  let LiInMenu = document.getElementsByClassName("menue");
+  for (let i = 0; i < LiInMenu.length; i++) {
+    LiInMenu[i].addEventListener("click", hiding);
+  }
