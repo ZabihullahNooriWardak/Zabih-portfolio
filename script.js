842252d4bdc9c33eb1a5f156e772d178ce264b06
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-06-27 13:21:19 +0430

    dry principle applied

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Dom manipulation For the burgerMenu
 
+//My Function For Dom manipulation of the burgerMenu
+
+let hiding = function () {
+  divBurgerMenueItem.style.display = "none";
+};
+
 // For showing the burger menu page
 
 let burgerMenu = document.querySelector(".bx-menu");
@@ -11,21 +17,16 @@ burgerMenu.addEventListener("click", function () {
 //for hiding the burgerMenu page using X
 
 let cross = document.querySelector(".bx-x");
-cross.addEventListener("click", function () {
-  divBurgerMenueItem.style.display = "none";
-});
+cross.addEventListener("click", hiding);
 
 // for hiding the burgerMenu page, clicking other parts of page
 
-let divAll=document.querySelector(".burger-menue");
-divAll.addEventListener('click',function(){
-    divBurgerMenueItem.style.display = "none";
-})
+let divAll = document.querySelector(".burger-menue");
+divAll.addEventListener("click", hiding);
+
 //for making the links in order to work
 
 let LiInMenu = document.getElementsByClassName("menue");
 for (let i = 0; i < LiInMenu.length; i++) {
-  LiInMenu[i].addEventListener('click',function(){
-      divBurgerMenueItem.style.display='none';
-  })
+  LiInMenu[i].addEventListener("click", hiding);
 }
