e5ddf58fa4ca95241d65a1852edddf4b9f540f2f
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-06-28 06:42:07 +0430

    js changed for the hiding when user click other parts of page with out X sign

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 // Dom manipulation For the burgerMenu
 
 // My Function For Dom manipulation of the burgerMenu
-const divBurgerMenueItem = document.querySelector('.burger-menue');
+const divBurgerMenueItem = document.querySelector(".burger-menue");
 
 const hiding = () => {
-  divBurgerMenueItem.style.display = 'none';
+  divBurgerMenueItem.style.display = "none";
 };
 
 // For showing the burger menu page
 
-const burgerMenu = document.querySelector('.bx-menu');
-burgerMenu.addEventListener('click', () => {
-  divBurgerMenueItem.style.display = 'flex';
+const burgerMenu = document.querySelector(".bx-menu");
+burgerMenu.addEventListener("click", () => {
+  divBurgerMenueItem.style.display = "flex";
 });
 
 // for hiding the burgerMenu page using X
 
-const cross = document.querySelector('.bx-x');
-cross.addEventListener('click', hiding);
-
-// for hiding the burgerMenu page, clicking other parts of page
-
-const divAll = document.querySelector('.burger-menue');
-divAll.addEventListener('click', hiding);
+const cross = document.querySelector(".bx-x");
+cross.addEventListener("click", hiding);
 
 // for making the links in order to work
 
-const LiInMenu = document.getElementsByClassName('menue');
+const LiInMenu = document.getElementsByClassName("menue");
 for (let i = 0; i < LiInMenu.length; i += 1) {
-  LiInMenu[i].addEventListener('click', hiding);
+  LiInMenu[i].addEventListener("click", hiding);
 }
